fe2cc2f33cc311ff2b9219a34169351033777f59
Jay <thellanajr@gmail.com>
2023-10-25 13:43:18 +0800

    created references on html and added digit display event listener

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 
+//create references to the buttons and display
+const display = document.getElementById('input');
+const digitButtons = document.getElementById('all-buttons');
+
+//create a function that populates the display when buttons clicked
+digitButtons.addEventListener('click', function(event) {
+    if (event.target.classList.contains('digits')) {
+        const digit = event.target.textContent;
+        addToDisplay(digit);
+    }
+});
+
+function addToDisplay(digit) {
+    if (display.value === "0") {
+        display.value = digit;
+    } else {
+        display.value += digit;
+    }
+}
+
 //create 3 variables to store the 2 digits and an operator
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 
-//create a function that populates the display when buttons clicked
+
+
 //create a function called operate that takes an operator and 2 numbers and then calls a function on the numbers
