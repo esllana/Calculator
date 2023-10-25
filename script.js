48bd58ff2f92102adf44e745ff4437adfa31d181
Jay <thellanajr@gmail.com>
2023-10-25 13:03:50 +0800

    added script comments. created initial variables

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+//create 3 variables to store the 2 digits and an operator
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+//create a function that populates the display when buttons clicked
+//create a function called operate that takes an operator and 2 numbers and then calls a function on the numbers
